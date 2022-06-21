467eae0199046ac46b8ca5f3d6015d246669f299
automatiek <arjan.kamphuis@gmail.com>
2022-06-21 12:49:57 +0200

    working fomo

--- source/ei_microphone.cpp
+++ source/ei_microphone.cpp
@@ -24,12 +24,15 @@
 
 #include "edge-impulse-sdk/CMSIS/DSP/Include/dsp/support_functions.h"
 #include "edge-impulse-sdk/dsp/memory.hpp"
-#include "firmware-sdk/ei_microphone_lib.h"
+#include "firmware-sdk-alif/ei_microphone_lib.h"
+#include "firmware-sdk-alif/at_base64_lib.h"
 #include "edge-impulse-sdk/dsp/ei_utils.h"
+#include "edge-impulse-sdk/dsp/image/processing.hpp"
+#include "image_processing.h"
 
 extern "C"{
-#include "hal/Driver_SAI.h"
-#include "hal/Driver_PINMUX_AND_PINPAD.h"
+#include "Driver_SAI.h"
+#include "Driver_PINMUX_AND_PINPAD.h"
 }
 
 //TODO: use multiply of memory block size
@@ -166,6 +169,64 @@ public:
 };
 
 static EiMicrophoneAlif micAlif;
+
+uint8_t raw_image[CIMAGE_X*CIMAGE_Y*RGB_BYTES + 0x460];
+uint8_t rgb_image[CIMAGE_X*CIMAGE_Y*RGB_BYTES];
+
+class EiCameraAlif : public EiCamera
+{
+    virtual bool ei_camera_capture_rgb888_packed_big_endian(
+        uint8_t *image,
+        uint32_t image_size) override
+    {
+        while(camera_start(CAMERA_MODE_SNAPSHOT, raw_image) != 0);
+        camera_vsync(100); // RGB conversion and frame resize 
+        bayer_to_RGB(raw_image+0x460, rgb_image);
+        ei::image::processing::crop_and_interpolate_rgb888(
+            rgb_image,// const uint8_t *srcImage,
+            560,// int srcWidth,
+            560,// int srcHeight,
+            rgb_image,// uint8_t *dstImage,
+            96,// int dstWidth,
+            96// int dstHeight
+        );
+        memcpy(image, rgb_image, image_size);
+        return true;
+    }
+
+    /**
+     * @brief Get the list of supported resolutions, ie. not requiring
+     * any software processing like crop or resize
+     * 
+     * @param res pointer to store the list of resolutions
+     * @param res_num pointer to a variable that will contain size of the res list
+     */
+    virtual void get_resolutions(const ei_device_snapshot_resolutions_t **res, uint8_t *res_num) override
+    {
+
+        static ei_device_snapshot_resolutions_t snapshot_resolutions[] =
+            { { 96,96 } };
+
+        *res = snapshot_resolutions;
+        *res_num = ARRAY_LENGTH(snapshot_resolutions);
+    }
+
+    virtual ei_device_snapshot_resolutions_t get_min_resolution(void)
+    {
+        return { 96,96 };
+    }
+
+    virtual bool set_resolution(const ei_device_snapshot_resolutions_t res) override
+    {
+        return true;
+    }
+};
+
+EiCamera* EiCamera::get_camera()
+{
+    static EiCameraAlif cam;
+    return &cam;
+}
 
 int ei_microphone_init(int idx)
 {
